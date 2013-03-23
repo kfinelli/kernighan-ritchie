dc629cf29682d0f8dedda626a43586016d03a6fa
Kevin Finelli <k.finelli@gmail.com>
2013-03-23 23:22:04 +0100

    17

---
+++ ch1/print-long-line.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#define MAXLINE 1000 /*max input line size*/
+#define CUTLEN 80 /*minimum length to print lines*/
+
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+/*exercise 1.17*/
+/*print lines longer than 80 characters*/
+main() {
+  int len;     /*store length for extra-long lines*/
+  int thislen; /*current line length*/
+  int max; /*maximum length seen so far */
+  char line[MAXLINE]; /*current input line*/
+  char longline[MAXLINE]; /*temprorarily store first part of a long
+			    line in case it passes the cut*/
+  char longLine = 0; /*program state - are we splitting a long line to
+		       copy it?*/
+  max = 0, len = 0;
+  while ((thislen = getline(line, MAXLINE)) > 0) {
+    len += thislen;
+    if (thislen == MAXLINE-1) { /*limited by maxline size */
+      if (!longLine) {
+	longLine = 1;
+	copy(longline, line); /*only copy the first part of long
+				lines*/
+      }
+      continue;
+    }
+    if (len > CUTLEN) {
+      if (!longLine) /*only copy the first part of long lines*/
+	printf("%s", line);
+      else
+	printf("%s\n", longline);
+
+    }
+    len = 0;
+    longLine = 0;
+  }
+  return 0;
+}
+
+/*getline: read a lone into s, return length */
+int getline(char s[], int lim) {
+  int c, i;
+  for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+    s[i] = c;
+  if (c == '\n') {
+    s[i] = c;
+    ++i;
+  }
+  s[i] = '\0';
+  return i;
+}
+
+/*copy: copy `from' into `to'; assume to is big enough */
+void copy(char to[], char from[]) {
+  int i;
+
+  i = 0;
+  while((to[i] = from[i]) != '\0')
+    ++i;
+}
