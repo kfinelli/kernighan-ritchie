78a55046af23ff844fd098dcb32144a7ef83979c
Kevin Finelli <k.finelli@gmail.com>
2013-03-30 19:59:39 +0100

    exercise 23, done, 1 more to go

---
+++ ch1/strip_c_comments.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#define MAXLINE 1000 /*max input line size*/
+
+/*exercise 1.23*/
+/*Write a program to remove comments from a c program.  */
+
+/*program states*/
+const int NOMINAL = 0;
+const int QUOTE = 1;
+const int CHAR = 2;
+const int COMMENT = 3;
+
+main() {
+  int state = NOMINAL;
+  char c, prev = '\0';
+
+  while ((c = getchar())!=EOF) {
+    if (state == COMMENT) {
+      if (c == '/' && prev == '*') {
+	state = NOMINAL;
+	prev = '\0';
+      }
+      continue;
+    }
+    if (c == '*' && prev == '/') {
+      if (state == NOMINAL) {
+	state = COMMENT;
+	prev = c;
+	continue;
+      }
+    }
+    else if (c == '"') {
+      if (state == NOMINAL) { 
+	state = QUOTE;
+      }
+      else if (state == QUOTE) {
+	state = NOMINAL;
+      }
+    }
+    else if (c == '\'') {
+      if (state == NOMINAL ) {
+	state = CHAR;
+      }
+      else if (state == CHAR && prev != '\\') {
+	state = NOMINAL;
+      }
+    }
+    else if (c == '/') {
+      prev = c;
+      continue;
+    }
+    if (prev == '/') {
+      putchar(prev);
+    }
+    putchar(c);
+    prev = c;
+  }
+  return 0;
+}
+
